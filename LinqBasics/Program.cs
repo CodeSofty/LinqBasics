cd2080353047ba88da827b55e8ad13a77f016679
CodeSoftly <eclittlejohn@yahoo.com>
2023-12-29 02:02:23 -0600

    first commit, linq queries

---
+++ LinqBasics/Program.cs
@@ -0,0 +1,19 @@
+﻿// Specify the data source
+List<int> scores = new List<int> {97, 92, 81, 60};
+
+//Define the query expression.
+IEnumerable<string> scoreQuery =
+    (from score in scores
+    where score > 80
+    orderby score ascending 
+    select $"score is {score}");
+    
+Console.WriteLine(scoreQuery.Count());
+ 
+ //Execute the query.
+foreach (string s in scoreQuery)
+{
+    Console.WriteLine(s);
+}
+
+
